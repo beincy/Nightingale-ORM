7a46bdae39630cff1d7ec4a972cca5dfa93290cb
卞辉 <bianhui0524@sina.com>
2019-04-29 18:00:58 +0800

    修改元类

--- NightingaleORM/dbmodel.py
+++ NightingaleORM/dbmodel.py
@@ -20,19 +20,25 @@ class ModelMetaClass(type):
     def __new__(cls, name, bases, attrs):
         if name == "Model":
             return type.__new__(cls, name, bases, attrs)
-        tableName = attrs.get('__table__', None) or name.replace('Model','')
-        mappings = dict()
-        fields = []
-        primaryKey = None
+        dateBaseType=attrs.get('__dbType__', None)#数据库类型
+        dateBaseName = attrs.get('__dateBase__', None)#数据库名称
+        schemaName = attrs.get('__schema__', None) #如果有schema，schema的名称
+        tableName = attrs.get('__table__', None) or name.replace('Model','').lower()#表名称，如果没有默认实体去掉Model
+        
+        mappings = dict()#属性参照
+        fields = []#字段
+        primaryKey = None#主键
+
         for k, v in attrs.items():
             if isinstance(v, Field):
                 mappings[k] = v
                 if v.primary_key:
                     if primaryKey:
-                        raise RuntimeError(
-                            "Douplicate primary key for field :%s" % primaryKey)
+                        raise RuntimeError("Douplicate primary key for field :%s" % primaryKey)
                     primaryKey = k
                 else:
                     fields.append(k)
+                v=v.default
+                
         if not primaryKey:
             raise RuntimeError("Primary key not found")
