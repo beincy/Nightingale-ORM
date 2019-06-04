43dc87a18e2a20d2d2538a86530f5fffcbb5e92f
卞辉 <bianhui0524@sina.com>
2019-06-04 10:34:23 +0800

    新增修改

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from NightingaleORM.dbmodel import Model
 from NightingaleORM.fields import StringField,IntegerField,DateTimeField,FloatField
-import datetime,json
+import datetime,json,ujson
+from PYLINQ import PYLINQ
 
 
 class TaskModel(Model):
@@ -104,7 +105,16 @@ def mian():
     # print(perm)
     # print(len(perm))
     # print (json.dumps(ee,cls=ComplexEncoder))
-
+    mylist=[{"name":"卞辉","age":18,"size":41}
+    ,{"name":"袁伟","age":2,"size":41}
+    ,{"name":"赵雪峰","age":50,"size":41}
+    ,{"name":"姜坤","age":18,"size":42}
+    ,{"name":"国豪","age":2,"size":41}]
+    a=PYLINQ(mylist).where(lambda x:x['age']>1).skip(4)
+    print(a)
+    # print(ujson.dumps(a,ensure_ascii=False,indent=4))
+    for item in a:
+        print(ujson.dumps(item,ensure_ascii=False,indent=4))
 
 if __name__ == "__main__":
     mian()
